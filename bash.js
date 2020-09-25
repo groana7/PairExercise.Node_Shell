17703899128899986091c115cfc76a41e77c85c3
groana <groana7@gmail.com>
2020-09-25 13:15:15 -0400

    up to install nodemon

---
+++ bash.js
@@ -0,0 +1,25 @@
+// what is this??? process.stdout.write
+// how do we see the methods to stdout? ie: write, on
+// similar to using chrome dev tools in the browser
+
+// how can we console.log() in the terminal?
+
+
+// this creates a prompt function
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    // why are we trimming?
+    // we console logged without trim and it worked
+    const cmd = data.toString().trim();
+
+    if (cmd === 'pwd') {
+        // console.log('YAYYYY') // this works
+        process.stdout.write(process.cwd())
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+});
+
+
