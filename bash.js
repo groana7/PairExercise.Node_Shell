df559cd8d2abbc96d9816ec74efb36952a56aee2
Maria Quadriq <mquadri23@gmail.com>
2020-09-25 14:31:23 -0500

    refactored pwd.js

--- bash.js
+++ bash.js
@@ -14,7 +14,6 @@ process.stdin.on('data', (data) => {
 
     if (cmd === 'pwd') {
         require('./pwd');
-        //process.stdout.write(process.cwd())
     } else {
         process.stdout.write('You typed: ' + cmd);
         process.stdout.write('\nprompt > ');
--- pwd.js
+++ pwd.js
@@ -1,18 +1,3 @@
-// this creates a prompt function
-//process.stdout.write('prompt > ');
 
- process.stdin.on('data', (data) => {
-    // why are we trimming?
-    // we console logged without trim and it worked
-    const cmd = data.toString().trim();
-
-    if (cmd === 'pwd') {
-        // console.log('YAYYYY') // this works
-        process.stdout.write(process.cwd())
-    } else {
-        process.stdout.write('You typed: ' + cmd);
-        process.stdout.write('\nprompt > ');
-    }
-});
 
 module.exports = process.stdout.write(process.cwd())
