e8d3de5d6dd135bcccacdfa37d8b452344bdca77
Maria Quadriq <mquadri23@gmail.com>
2020-09-25 14:25:18 -0500

    pwd.js file created

--- bash.js
+++ bash.js
@@ -9,17 +9,17 @@
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
-    // why are we trimming?
-    // we console logged without trim and it worked
-    const cmd = data.toString().trim();
+    
+    const cmd = data.toString().trim()
 
     if (cmd === 'pwd') {
-        // console.log('YAYYYY') // this works
-        process.stdout.write(process.cwd())
+        require('./pwd');
+        //process.stdout.write(process.cwd())
     } else {
         process.stdout.write('You typed: ' + cmd);
         process.stdout.write('\nprompt > ');
     }
 });
+
 
 
